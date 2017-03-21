0ba3429c33c4fb7d75181a2f05a5372fe292d0a1
Connor <cbroyles94@gmail.com>
2017-03-21 01:00:03 -0400

    Fixed readability of settings.json

--- bot.js
+++ bot.js
@@ -34,10 +34,11 @@ function toggleNSFW(message, setting) {
       settings[server].NSFW = setting;
     }
 
-    var json = JSON.stringify(settings);
+    var json = JSON.stringify(settings, null, "\t");
     fs.writeFile('./config/settings.json', json, 'utf8', function(err) {
       if (err) throw err});
 
+    settings = require('./config/settings.json');
     console.log("Server: " + message.guild.name + " - " + message.guild.id + "\nNSFW: " + setting + "\n");
   }
   else {
@@ -160,7 +161,7 @@ client.on('message', (message) => {
   if (message.content == "ping") {
         message.channel.sendMessage("pong");
   }
-  else if (message.content == "navy weeb") {
+  else if (message.content.indexOf("navy weeb") !== -1) {
         message.channel.sendMessage("Nani the fuck did you just fucking iimasu about watashi, you chiisai bitch desuka? Watashi’ll have anata know that watashi graduated top of my class in Nihongo 3, and watashi’ve been involved in iroirona Nihongo tutoring sessions, and watashi have over sanbyaku perfect test scores. Watashi am trained in kanji, and watashi is the top letter writer in all of southern California. Anata are nothing to watashi but just another weaboo. Watashi will korosu anata the fuck out with vocabulary the likes of which has never been mimasu’d before on this continent, mark watashino fucking words. Anata thinks anata can get away with hanashimasing that kuso to watashi over the intaaneto? Omou again, fucker. As we hanashimasu, watashi am contacting watashino secret netto of otakus across the USA, and anatano IP is being traced right now so you better junbishimasu for the ame, ujimushi. The ame that korosu’s the pathetic chiisai thing anata calls anatano life. You’re fucking shinimashita’d, akachan.");
   }
 });
