0be5892743fb389fe6bc2f7aab6fb7c249c4b3a2
Connor <cbroyles94@gmail.com>
2017-03-24 17:32:38 -0400

    Settings file is now automatically created Posts id is limited to 1000 (slower load times the higher it is) Bot replies to user if a post couldn't be found

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const waifu    = require('./modules/waifu.js');
 const oauth2   = require('./config/oauth2.json');
 const baseTags = require('./config/basetags.json').tags;
 const client   = new Discord.Client();
+
 if (fs.existsSync('./config/settings.json')) {
   var settings   = require('./config/settings.json');
 } else {
@@ -11,7 +12,6 @@ if (fs.existsSync('./config/settings.json')) {
     if (err) throw err});
   var settings   = require('./config/settings.json');
 }
-
 
 
 //Ready notification
@@ -52,17 +52,36 @@ function toggleNSFW(message, setting) {
   }
 }
 
-function sendWaifu(message, tags, messageText) {
-  if (!settings[message.guild.id] || !settings[message.guild.id]['NSFW']) tags.push("rating:safe");
+
+async function sendWaifu(message, tags, messageText) {
+  if (!message.guild || !settings[message.guild.id] || !settings[message.guild.id]['NSFW']) {
+    tags.push("rating:safe");
+  }
+
+  //get username if DM or displayName if server
   var username = message.member ? message.member.displayName : message.author.username;
-  waifu.deliverWaifu(tags)    //get random waifu with specified tags
-       .then(image => {       //send message
-         message.channel.sendEmbed({image: {url: `http:${image[0].file_url}`},
-                                    color: 3447003,
-                                    title: username + ', ' + `your ${messageText} is ${image[0].name}`,
-                                    description: `http://gelbooru.com/index.php?page=post&s=view&id=${image[0].id}`});
-       });
+
+  var image = await waifu.deliverWaifu(tags);
+  if (image) {
+    message.channel.sendEmbed({image: {url: `http:${image[0].file_url}`},
+                               color: 3447003,
+                               title: username + ', ' + `your ${messageText} is ${image[0].name}`,
+                               description: `http://gelbooru.com/index.php?page=post&s=view&id=${image[0].id}`});
+  }
+  else {
+    message.reply("Could not find an image. Check your tags if applicable.")
+  }
+  // waifu.deliverWaifu(tags)    //get random waifu with specified tags
+  //      .then(image => {       //send message
+  //        message.channel.sendEmbed({image: {url: `http:${image[0].file_url}`},
+  //                                   color: 3447003,
+  //                                   title: username + ', ' + `your ${messageText} is ${image[0].name}`,
+  //                                   description: `http://gelbooru.com/index.php?page=post&s=view&id=${image[0].id}`});
+  //      })
+  //      .catch(message.reply("Could not find an image. Check your tags if applicable."))
+
 }
+
 
 function sendHelp(message) {
   message.channel.send("__**Commands:**__\n"+
@@ -78,6 +97,7 @@ function sendHelp(message) {
                        "**@" + client.user.username + " aliases file:** - Send a DM with the aliases file. Might be easier to read.");
 }
 
+
 function sendAliasesDM(message) {
   var aliasList = waifu.stringifyAliases();
   message.author.send("__**Tag Aliases:**__\n");
@@ -86,6 +106,7 @@ function sendAliasesDM(message) {
     message.author.send(list);
   }
 }
+
 
 function sendAliasesFile (message) {
   message.author.sendFile('./config/aliases.json');
@@ -96,65 +117,64 @@ function sendAliasesFile (message) {
 *        MAIN COMMANDS       *
 *****************************/
 client.on('message', (message) => {
-  if (!message.author.bot) {
+  if (message.author.bot) return;
 
-    //waifu
-    if (message.content.indexOf("waifu") !== -1 && !message.content.startsWith("@Waifubot")) {
-      if (message.content.startsWith("waifu")) {                          //treat next words as tags if first word is waifu
-        var tags = baseTags.concat(message.content.slice(6).split(" "));  //separate tags
-        tags = tags.filter(tag => {return tag != '';});                   //remove empty tags
-      } else {
-        var tags = baseTags;  //only use standard tags otherwise
-      }
-
-      var messageText = "waifu";
-      sendWaifu(message, tags, messageText);
+  //waifu
+  if (message.content.toLowerCase().indexOf("waifu") !== -1 && !message.content.startsWith("@Waifubot")) {
+    if (message.content.toLowerCase().startsWith("waifu")) {            //treat next words as tags if first word is waifu
+      var tags = baseTags.concat(message.content.slice(6).split(" "));  //separate tags
+      tags = tags.filter(tag => {return tag != '';});                   //remove empty tags
+    } else {
+      var tags = baseTags;  //only use standard tags otherwise
     }
 
-    //monstergirl
-    else if (message.content.indexOf("monstergirl") !== -1) {
-      var tags = baseTags.concat("monster_musume_no_iru_nichijou");
-      var messageText = "monstergirl";
-      sendWaifu(message, tags, messageText);
+    var messageText = "waifu";
+    sendWaifu(message, tags, messageText);
+  }
+
+  //monstergirl
+  else if (message.content.toLowerCase().indexOf("monstergirl") !== -1) {
+    var tags = baseTags.concat("monster_musume_no_iru_nichijou");
+    var messageText = "monstergirl";
+    sendWaifu(message, tags, messageText);
+  }
+
+  //shipgirl
+  else if (message.content.toLowerCase().indexOf("shipgirl") !== -1) {
+    var tags = baseTags.concat("kantai_collection");
+    var messageText = "shipgirl";
+    sendWaifu(message, tags, messageText);
+  }
+
+  //tankgirl
+  else if (message.content.toLowerCase().indexOf("tankgirl") !== -1) {
+    var tags = baseTags.concat("girls_und_panzer");
+    var messageText = "tankgirl";
+    sendWaifu(message, tags, messageText);
+  }
+
+  //help message
+  else if (message.isMentioned(client.user)) {
+    if (message.content.indexOf("help") !== -1) {
+      sendHelp(message);
     }
 
-    //shipgirl
-    else if (message.content.indexOf("shipgirl") !== -1) {
-      var tags = baseTags.concat("kantai_collection");
-      var messageText = "shipgirl";
-      sendWaifu(message, tags, messageText);
+    else if (message.content.indexOf("nsfw on") !== -1) {
+      toggleNSFW(message, true);
     }
 
-    //tankgirl
-    else if (message.content.indexOf("tankgirl") !== -1) {
-      var tags = baseTags.concat("girls_und_panzer");
-      var messageText = "tankgirl";
-      sendWaifu(message, tags, messageText);
+    else if (message.content.indexOf("nsfw off") !== -1) {
+      toggleNSFW(message, false);
     }
 
-    //help message
-    else if (message.isMentioned(client.user)) {
-      if (message.content.indexOf("help") !== -1) {
-        sendHelp(message);
-      }
+    //send alias list
+    else if (message.content.indexOf("aliases DM") !== -1) {
+      sendAliasesDM(message);
+    }
 
-      else if (message.content.indexOf("nsfw on") !== -1) {
-        toggleNSFW(message, true);
-      }
-
-      else if (message.content.indexOf("nsfw off") !== -1) {
-        toggleNSFW(message, false);
-      }
-
-      //send alias list
-      else if (message.content.indexOf("aliases DM") !== -1) {
-        sendAliasesDM(message);
-      }
-
-      //send alias file
-      else if (message.content.indexOf("aliases file") !== -1) {
-        sendAliasesFile(message);
-      }
+    //send alias file
+    else if (message.content.indexOf("aliases file") !== -1) {
+      sendAliasesFile(message);
     }
   }
 });
@@ -167,7 +187,7 @@ client.on('message', (message) => {
   if (message.content == "ping") {
         message.channel.sendMessage("pong");
   }
-  else if (message.content.indexOf("navy weeb") !== -1) {
+  else if (message.content.toLowerCase().indexOf("navy weeb") !== -1) {
         message.channel.sendMessage("Nani the fuck did you just fucking iimasu about watashi, you chiisai bitch desuka? Watashi’ll have anata know that watashi graduated top of my class in Nihongo 3, and watashi’ve been involved in iroirona Nihongo tutoring sessions, and watashi have over sanbyaku perfect test scores. Watashi am trained in kanji, and watashi is the top letter writer in all of southern California. Anata are nothing to watashi but just another weaboo. Watashi will korosu anata the fuck out with vocabulary the likes of which has never been mimasu’d before on this continent, mark watashino fucking words. Anata thinks anata can get away with hanashimasing that kuso to watashi over the intaaneto? Omou again, fucker. As we hanashimasu, watashi am contacting watashino secret netto of otakus across the USA, and anatano IP is being traced right now so you better junbishimasu for the ame, ujimushi. The ame that korosu’s the pathetic chiisai thing anata calls anatano life. You’re fucking shinimashita’d, akachan.");
   }
 });
